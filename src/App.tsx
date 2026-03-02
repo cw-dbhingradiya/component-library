e924f844a44e961eb546dae52fbc27aaa97ab813
cw-dbhingradiya <dbhingradiya@codewinglet.com>
2026-03-02 11:01:25 +0530

    feat: remove unused file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LandingPage } from "./pages/landing-page";
-import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/" element={<div>Hello World</div>} />
       </Routes>
     </BrowserRouter>
   );
